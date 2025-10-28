f2d6919e03e9937cebf667914095d1ee3e62464e
paz1ch <danghungphucc@gmail.com>
2025-10-28 10:35:16 +0700

    add login user logic in user service

--- backend-microservices/user-service/src/controllers/user-controller.js
+++ backend-microservices/user-service/src/controllers/user-controller.js
@@ -2,7 +2,7 @@
 const User = require('../models/user');
 const generateTokens = require('../utils/generateToken');
 const logger = require('../utils/logger');
-const { validateRegistration } = require('../utils/validation');
+const { validateRegistration, validatelogin } = require('../utils/validation');
 
 
 //dang ky 
@@ -17,7 +17,8 @@ const registerUser = async (req, res) => {
                 message: error.details[0].message,
             });
         }
-        const { email, password, username } = req.body
+        const { email, password, username } = req.body;
+
         let user = await User.findOne({ $or: [{ email }, { username }] })
         if (user) {
             logger.warn("nguoi dung da ton tai");
@@ -53,6 +54,41 @@ const registerUser = async (req, res) => {
 const loginUser = async (req, res) => {
     logger.info("Login endpoint hit...");
     try {
+        const { error } = validatelogin(req.body);
+        if (error) {
+            logger.warn('Validation error', error.details[0].message);
+            return res.status(400).json({
+                success: false,
+                message: error.details[0].message,
+            });
+        }
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if(!user){
+            logger.warn("Invalid user");
+            return res.status(400).json({
+                success: false,
+                message: "Invalid credentials",
+            });      
+        }
+
+        //kiem tra mat khau co hop le khong
+        const isValidPassword = await user.comparePassword(password);
+        if(!isValidPassword){
+            logger.warn("Invalid password");
+            return res.status(400).json({
+                sucess : false,
+                message : "Invalid credentials"
+            })
+        }
+
+        const {accessToken, refreshToken } = await generateTokens(user);
+
+        res.json({
+            accessToken,
+            refreshToken,
+            userId : user._id
+        })
 
     } catch (e) {
         logger.error('Login error occured: ', e)
